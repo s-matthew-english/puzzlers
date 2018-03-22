1ba6ed517dfc61a2c60cc05af6095d052dfe67ea
s.matthew.english <s.matthew.english@gmail.com>
2018-03-22 10:57:09 -0400

    22.03.2018

---
+++ puzzle4/Elementary.java
@@ -0,0 +1,9 @@
+public class Elementary {
+  public static void main(String [] args) {
+   /*
+    * Always use a capital el (L) in `long` literals, 
+    * never a lowercase el (l).
+    */
+    System.out.println(12345 + 5432l);
+  }
+}
