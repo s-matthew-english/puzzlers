646fd1a7bdd6d699efd281ade3b43fb4bc8a09b0
s.matthew.english <s.matthew.english@gmail.com>
2018-04-03 00:53:18 -0400

    03.04.2018

---
+++ puzzle20/Me.java
@@ -0,0 +1,7 @@
+package puzzlers.puzzle20;
+public class Me {
+  public static void main(String[] args){
+      System.out.println(
+        Me.class.getName().replaceAll(".", "/") + ".class");
+  }
+}
