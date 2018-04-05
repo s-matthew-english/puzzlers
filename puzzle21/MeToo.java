6a34ca0b1dd779b23c69f3831e70d33e630d0032
s.matthew.english <s.matthew.english@gmail.com>
2018-04-05 11:21:24 -0400

    05.04.2018

---
+++ puzzle21/MeToo.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public class MeToo {
+  public static void main(String[] args) {
+    System.out.println(MeToo.class.getName().replaceAll("\\.", File.separator) + ".class");
+  }
+}
