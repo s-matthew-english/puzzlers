d918ee7a412f0e907c03e5d1bd2a19fa3377c3a2
S. Matthew English <s.matthew.english@gmail.com>
2018-06-09 06:20:44 -0400

    2018.06.09

---
+++ puzzle58/Name.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Name {
+	private final String first, last;
+
+	public Name(String first, String last) {
+		this.first = first; this.last = last;
+	}
+
+	// public boolean equals(Name n) {
+	// 	return n.first.equals(first) && n.last.equals(last);
+	// }
+
+	public boolean equals(Object o) {
+		if(!(o instanceof Name)) 
+			return false;
+		Name n = (Name)o;
+		return n.first.equals(first) && n.last.equals(last);
+	}
+
+	public int hashCode() {
+		return 31 * first.hashCode() + last.hashCode();
+	}
+
+	public static void main(String... args) {
+		Set<Name> s = new HashSet<Name>();
+		s.add(new Name("Donald", "Duck"));
+		System.out.println(s.contains(new Name("Donald", "Duck")));
+	}
+}
